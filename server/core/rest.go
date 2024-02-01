913285357ea4afbebe24b39f1e7408a28214e0a9
Christian Roessner <c@roessner.co>
2024-02-01 16:37:38 +0100

    Refactor flushCache function for improved readability
    The refactor involves breaking the flushCache function into smaller, reusable functions for better readability and maintainability. This includes the creation of helper functions that handle specific tasks related to flushing a user's cache, such as processing the flush cache, processing user commands, setting user keys, and removing a user from the cache. Furthermore, each newly created function is well-documented to enhance understanding of the operation being performed.

--- server/core/rest.go
+++ server/core/rest.go
@@ -16,6 +16,21 @@ import (
 
 // For a brief documentation of this file please have a look at the Markdown document REST-API.md.
 
+// FlushUserCmdStatus represents an user's command status.
+type FlushUserCmdStatus struct {
+	// User holds the identifier of a user.
+	User string `json:"user"`
+
+	// Status represents the status of the user's command.
+	Status string `json:"status"`
+}
+
+// FlushUserCmd is a data structure used to handle user commands for flushing data.
+type FlushUserCmd struct {
+	// User is the field representing the name of the user to be flushed.
+	User string `json:"user"`
+}
+
 // generic handles the generic authentication logic based on the selected service type.
 func (a *Authentication) generic(ctx *gin.Context) {
 	var mode string
@@ -156,40 +171,62 @@ func listBruteforce(ctx *gin.Context) {
 	})
 }
 
-//nolint:gocognit // Ignore
+// flushCache is a handler function for a Gin HTTP server. It takes a gin.Context as a parameter
+// and attempts to flush the cache according to the *FlushUserCmd in the request's JSON body.
+//
+// Parameters:
+//   - ctx:  A pointer to gin.Context. The context is used for retrieving a *FlushUserCmd
+//     payload from the request and for sending HTTP responses. The context also carries a
+//     globally unique identifier (GUID) for logging purposes.
+//
+// Local variables:
+//   - userCmd:   A pointer to a FlushUserCmd object. This object is populated with data from the
+//     request's JSON body.
+//   - guid:      The globally unique identifier retrieved from the context for logging.
+//   - useCache:  A flag indicating whether the cache backend is currently in use by the application.
+//     When true, the function can remove password history keys from the cache.
+//   - statusMsg: A variable for storing the status message. This message will be either "flushed"
+//     or "not flushed", based on the outcome of the cache flush operation.
+//
+// Procedure:
+//  1. The function first retrieves the GUID from the context.
+//  2. Then, it logs the GUID along with the flushing information.
+//  3. It attempts to bind the JSON payload from the request to a FlushUserCmd object.
+//  4. If any error occurs during this binding, the function logs the error and the GUID.
+//     After that, it aborts the current HTTP request by sending a 400 (Bad Request)
+//     status code as a response. Then the function returns.
+//  5. If there are no binding errors, the function processes the cache flush.
+//  6. Based on the useCache flag and the outcome of the cache flush operation, the function
+//     updates the statusMsg and sends the cache status to the client.
 func flushCache(ctx *gin.Context) {
-	type FlushUserCmdStatus struct {
-		User   string `json:"user"`
-		Status string `json:"status"`
-	}
-
-	type FlushUserCmd struct {
-		User string `json:"user"`
-	}
-
-	var (
-		cacheFlushError bool
-		useCache        bool
-		removeHash      bool
-		accountName     string
-		protocols       []string
-		err             error
-	)
-
 	guid := ctx.Value(global.GUIDKey).(string)
-	userKeys := config.NewStringSet()
-	statusMsg := "flushed"
 	userCmd := &FlushUserCmd{}
 
 	level.Info(logging.DefaultLogger).Log(global.LogKeyGUID, guid, global.CatCache, global.ServFlush)
 
-	if err = ctx.BindJSON(userCmd); err != nil {
+	if err := ctx.BindJSON(userCmd); err != nil {
 		level.Error(logging.DefaultErrLogger).Log(global.LogKeyGUID, guid, global.LogKeyError, err)
 		ctx.AbortWithStatus(http.StatusBadRequest)
 
 		return
 	}
 
+	cacheFlushError, useCache := processFlushCache(userCmd, guid)
+
+	statusMsg := "flushed"
+	if cacheFlushError {
+		statusMsg = "not flushed"
+	}
+
+	sendCacheStatus(ctx, guid, userCmd, useCache, statusMsg)
+}
+
+// processFlushCache takes a user command and a GUID and processes the cache flush.
+// It iterates through the PassDBs in the EnvConfig and checks if the backend is BackendCache.
+// If it is, it sets useCache to true and calls processUserCmd to process the user command.
+// If there is an error during the cache flush, cacheFlushError is set to true and the loop breaks.
+// It returns cacheFlushError and useCache flags.
+func processFlushCache(userCmd *FlushUserCmd, guid string) (cacheFlushError bool, useCache bool) {
 	for _, passDB := range config.EnvConfig.PassDBs {
 		if passDB.Get() != global.BackendCache {
 			continue
@@ -197,86 +234,171 @@ func flushCache(ctx *gin.Context) {
 
 		useCache = true
 
-		level.Info(logging.DefaultLogger).Log(global.LogKeyGUID, guid, "user", userCmd.User)
-
-		if userCmd.User == "*" {
-			accountName = "*"
-			removeHash = true
-		} else {
-			accountName, err = backend.LookupUserAccountFromRedis(userCmd.User)
-			if err != nil {
-				cacheFlushError = true
-
-				break
-			}
-
-			// User not known in positive password cache. Fallback to requested address.
-			if accountName == "" {
-				accountName = userCmd.User
-			}
-		}
-
-		// Make sure that the requested address was not empty.
-		if accountName == "" {
-			cacheFlushError = true
-
+		cacheFlushError = processUserCmd(userCmd, guid)
+		if cacheFlushError {
 			break
 		}
+	}
 
-		userKeys.Set(config.EnvConfig.RedisPrefix + "ucp:__default__:" + accountName)
-		userKeys.Set(config.EnvConfig.RedisPrefix + global.RedisPwHashKey + ":" + userCmd.User + ":*")
+	return cacheFlushError, useCache
+}
 
-		protocols = config.LoadableConfig.GetAllProtocols()
+// processUserCmd processes the user command by performing the following steps:
+// 1. Calls the setupCacheFlush function to set up the cache flush and retrieve the account name, removeHash flag, and cacheFlushError flag.
+// 2. If cacheFlushError is true, returns true immediately.
+// 3. Calls the setUserKeys function to set the user keys using the user command and account name.
+// 4. Calls the removeUserFromCache function to remove the user from the cache by providing the user command, user keys, guid, and removeHash flag.
+// 5. Returns false.
+func processUserCmd(userCmd *FlushUserCmd, guid string) bool {
+	accountName, removeHash, cacheFlushError := setupCacheFlush(userCmd)
+	if cacheFlushError {
+		return true
+	}
 
-		for index := range protocols {
-			cacheNames := backend.GetCacheNames(protocols[index], global.CacheAll)
+	userKeys := setUserKeys(userCmd, accountName)
 
-			for _, cacheName := range cacheNames.GetStringSlice() {
-				userKeys.Set(config.EnvConfig.RedisPrefix + "ucp:" + cacheName + ":" + accountName)
-			}
+	removeUserFromCache(userCmd, userKeys, guid, removeHash)
+
+	return false
+}
+
+// setupCacheFlush handles the caching flush logic based on the provided user command.
+// It checks if the user command is a wildcard ("*"), in which case it returns "*", true, false.
+// If the user command is not a wildcard, it looks up the user account name using the backend.LookupUserAccountFromRedis function.
+// If there is an error during the lookup or the account name is empty, it returns "", false, true.
+// Otherwise, it returns the account name, false, false.
+//
+// Example usage:
+//
+//	userCmd := &FlushUserCmd{
+//		User: "john.doe",
+//	}
+//	accountName, removeHash, cacheFlushError := setupCacheFlush(userCmd)
+//	if cacheFlushError {
+//		// Handle cache flush error
+//	}
+//	// Continue with cache flushing logic
+//
+// Note: The `FlushUserCmd` type is defined as follows:
+//
+//	type FlushUserCmd struct {
+//		User string `json:"user"`
+//	}
+//
+// Note: The `backend.LookupUserAccountFromRedis` function is defined as follows:
+//
+//	func LookupUserAccountFromRedis(username string) (accountName string, err error) {
+//		// ...
+//	}
+func setupCacheFlush(userCmd *FlushUserCmd) (string, bool, bool) {
+	if userCmd.User == "*" {
+		return "*", true, false
+	}
+
+	accountName, err := backend.LookupUserAccountFromRedis(userCmd.User)
+	if err != nil || accountName == "" {
+		return "", false, true
+	}
+
+	return accountName, false, false
+}
+
+// setUserKeys populates a string set with user keys based on the given FlushUserCmd and accountName.
+// The function creates a new empty string set using the NewStringSet function from the config package.
+// It then sets two keys in the string set: one is a concatenation of the RedisPrefix constant from the config package,
+// "ucp:__default__:", and the accountName parameter. The other key is a concatenation of the RedisPrefix constant,
+// the RedisPwHashKey constant from the global package, ":", the User field from the userCmd parameter, and ":*".
+// Next, it iterates over the protocols obtained from the LoadableConfig.GetAllProtocols function from the config package.
+// For each protocol, it retrieves the cache names using the backend.GetCacheNames function from the backend package,
+// passing the protocol and the global.CacheAll constant. For each cache name, it sets a key in the string set
+// by concatenating the RedisPrefix constant, "ucp:", the cache name, ":", and the accountName parameter.
+// Finally, the function returns the populated string set.
+func setUserKeys(userCmd *FlushUserCmd, accountName string) config.StringSet {
+	userKeys := config.NewStringSet()
+
+	userKeys.Set(config.EnvConfig.RedisPrefix + "ucp:__default__:" + accountName)
+	userKeys.Set(config.EnvConfig.RedisPrefix + global.RedisPwHashKey + ":" + userCmd.User + ":*")
+
+	protocols := config.LoadableConfig.GetAllProtocols()
+	for index := range protocols {
+		cacheNames := backend.GetCacheNames(protocols[index], global.CacheAll)
+		for _, cacheName := range cacheNames.GetStringSlice() {
+			userKeys.Set(config.EnvConfig.RedisPrefix + "ucp:" + cacheName + ":" + accountName)
 		}
+	}
 
-		// Remove user from hash map.
-		redisKey := config.EnvConfig.RedisPrefix + global.RedisUserHashKey
+	return userKeys
+}
 
-		if removeHash {
-			// User command is a wildcard.
-			if err = backend.RedisHandle.Del(backend.RedisHandle.Context(), redisKey).Err(); err != nil {
-				level.Error(logging.DefaultErrLogger).Log(global.LogKeyGUID, guid, global.LogKeyError, err)
+// removeUserFromCache removes a user from the cache based on the given parameters.
+// If removeHash is true, it deletes the entire Redis hash map associated with the user.
+// Otherwise, it only removes the specific user key from the hash map.
+// It also deletes other user keys stored in the userKeys string set.
+// If any error occurs during the removal process, it logs the error and immediately returns.
+// After successful removal, it logs the keys that have been flushed.
+func removeUserFromCache(userCmd *FlushUserCmd, userKeys config.StringSet, guid string, removeHash bool) {
+	var err error
 
-				cacheFlushError = true
+	redisKey := config.EnvConfig.RedisPrefix + global.RedisUserHashKey
 
-				break
-			}
-		} else if err = backend.RedisHandle.HDel(backend.RedisHandle.Context(), redisKey, userCmd.User).Err(); err != nil {
-			// User command is a specific user.
+	if removeHash {
+		err = backend.RedisHandle.Del(backend.RedisHandle.Context(), redisKey).Err()
+	} else {
+		err = backend.RedisHandle.HDel(backend.RedisHandle.Context(), redisKey, userCmd.User).Err()
+	}
+
+	if err != nil {
+		level.Error(logging.DefaultErrLogger).Log(global.LogKeyGUID, guid, global.LogKeyError, err)
+
+		return
+	}
+
+	for _, userKey := range userKeys.GetStringSlice() {
+		if _, err = backend.RedisHandle.Del(backend.RedisHandle.Context(), userKey).Result(); err != nil {
 			level.Error(logging.DefaultErrLogger).Log(global.LogKeyGUID, guid, global.LogKeyError, err)
 
-			cacheFlushError = true
-
-			break
+			return
 		}
 
-		// Remove user associated object from ucp-namespace(s).
-		for _, userKey := range userKeys.GetStringSlice() {
-			if _, err = backend.RedisHandle.Del(backend.RedisHandle.Context(), userKey).Result(); err != nil {
-				level.Error(logging.DefaultErrLogger).Log(global.LogKeyGUID, guid, global.LogKeyError, err)
-
-				cacheFlushError = true
-
-				break
-			}
-
-			level.Info(logging.DefaultLogger).Log(global.LogKeyGUID, guid, "keys", userKey, "status", "flushed")
-		}
-
-		break
+		level.Info(logging.DefaultLogger).Log(global.LogKeyGUID, guid, "keys", userKey, "status", "flushed")
 	}
+}
 
-	if cacheFlushError {
-		statusMsg = "not flushed"
-	}
-
+// sendCacheStatus is a function that sends the cache status as a response to the client.
+// If the useCache parameter is true, it sends a JSON response with the cache status message and the user command details.
+// If useCache is false, it sends a JSON response with an error message indicating that the cache backend is not enabled.
+//
+// Parameters:
+// - ctx: The gin.Context object representing the HTTP request and response context.
+// - guid: The GUID string associated with the request.
+// - userCmd: A pointer to a FlushUserCmd object containing user command details.
+// - useCache: A boolean indicating whether the cache backend is enabled or not.
+// - statusMsg: The status message to be included in the response.
+//
+// Example usage:
+//
+//	func flushCache(ctx *gin.Context) {
+//	   guid := ctx.Value(global.GUIDKey).(string)
+//	   userCmd := &FlushUserCmd{}
+//
+//	   level.Info(logging.DefaultLogger).Log(global.LogKeyGUID, guid, global.CatCache, global.ServFlush)
+//
+//	   if err := ctx.BindJSON(userCmd); err != nil {
+//	       level.Error(logging.DefaultErrLogger).Log(global.LogKeyGUID, guid, global.LogKeyError, err)
+//	       ctx.AbortWithStatus(http.StatusBadRequest)
+//	       return
+//	   }
+//
+//	   cacheFlushError, useCache := processFlushCache(userCmd, guid)
+//
+//	   statusMsg := "flushed"
+//	   if cacheFlushError {
+//	       statusMsg = "not flushed"
+//	   }
+//
+//	   sendCacheStatus(ctx, guid, userCmd, useCache, statusMsg)
+//	}
+func sendCacheStatus(ctx *gin.Context, guid string, userCmd *FlushUserCmd, useCache bool, statusMsg string) {
 	if useCache {
 		level.Info(logging.DefaultLogger).Log(global.LogKeyGUID, guid, global.LogKeyMsg, statusMsg)
 
