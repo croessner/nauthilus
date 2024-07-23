917b6622bee16a846167d4482e53a451193ce2dc
Christian Roessner <c@roessner.co>
2024-07-23 12:40:26 +0200

    Extend default Lua package path
    The default Lua package path has been extended with a new location. The function `PackagePath` in `packagepath.go` now also includes the path "/usr/app/lua-plugins.d/share/?.lua" by default. This provides an additional location to source Lua scripts for the application.
    
    Signed-off-by: Christian Roessner <c@roessner.co>

--- server/lualib/packagepath.go
+++ server/lualib/packagepath.go
@@ -11,7 +11,7 @@ import (
 // It appends the Lua package path with the value returned by `config.LoadableConfig.GetLuaPackagePath()`.
 // This function takes a Lua state (`*lua.LState`) as an argument and returns an error.
 func PackagePath(L *lua.LState) error {
-	defaultPath := "/usr/local/share/nauthilus/lua/?.lua;/usr/share/nauthilus/lua/?.lua;"
+	defaultPath := "/usr/local/share/nauthilus/lua/?.lua;/usr/share/nauthilus/lua/?.lua;/usr/app/lua-plugins.d/share/?.lua"
 
 	return L.DoString(fmt.Sprintf(`package.path = package.path .. ';%s;%s'`, defaultPath, config.LoadableConfig.GetLuaPackagePath()))
 }
