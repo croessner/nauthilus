10e8ae7fd08d29a4d6497b114733d5accb47f3a8
Christian Roessner <c@roessner.co>
2024-08-21 15:37:29 +0200

    Refactor documentation for SetUPRedisFunctions
    Updated the comments for the SetUPRedisFunctions method to provide clearer and more detailed descriptions of the Redis function mappings. This includes specifying the purposes of each function and adding documentation for additional Redis commands.
    
    Signed-off-by: Christian Roessner <c@roessner.co>

--- server/lualib/redislib/register.go
+++ server/lualib/redislib/register.go
@@ -5,21 +5,30 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
-// SetUPRedisFunctions is a function that associates a set of Redis-related functions to a Lua table.
-// Each function is linked to a string that corresponds to its name in the global Lua functions namespace.
-// The provided Lua state `L` and the Lua table `table` are used to facilitate this setting up process.
-// Here are the functions that this function sets up:
-// - RedisGet
-// - RedisSet
-// - RedisIncr
-// - RedisDel
-// - RedisExpire
-// - RedisHGet
-// - RedisHSet
-// - RedisHDel
-// - RedisHLen
-// - RedisHGetAll
-// - RedisHIncrBy
+// SetUPRedisFunctions sets up the Redis functions in the Lua table.
+// It takes a Lua table and a Lua state as arguments.
+// It assigns the Redis functions to the corresponding keys in the Lua table.
+// Each Redis function is implemented as a Lua function that interacts with the Redis server.
+// The Redis functions include:
+//   - RedisGet: Retrieves the value associated with a key from the Redis server.
+//   - RedisSet: Sets the value of a key in the Redis server.
+//   - RedisIncr: Increments the value associated with a key in the Redis server.
+//   - RedisDel: Deletes the value associated with a key from the Redis server.
+//   - RedisExpire: Sets a timeout on a key in the Redis server.
+//   - RedisHGet: Retrieves the value associated with a field in a Hash stored at a key in the Redis server.
+//   - RedisHSet: Sets the value of a field in a Hash stored at a key in the Redis server.
+//   - RedisHDel: Deletes a field from a Hash stored at a key in the Redis server.
+//   - RedisHLen: Returns the number of fields in a Hash stored at a key in the Redis server.
+//   - RedisHGetAll: Returns all the fields and values in a Hash stored at a key in the Redis server.
+//   - RedisHIncrBy: Increments the value of a field in a Hash stored at a key in the Redis server by a specified amount.
+//   - RedisHIncrByFloat: Increments the value of a field in a Hash stored at a key in the Redis server by a specified floating-point number.
+//   - RedisHExists: Checks if a field exists in a Hash stored at a key in the Redis server.
+//   - RedisRename: Renames a key in the Redis server.
+//   - RedisSAdd: Adds one or more members to a set stored at a key in the Redis server.
+//   - RedisSIsMember: Checks if a member is a member of a set stored at a key in the Redis server.
+//   - RedisSMembers: Returns all the members of a set stored at a key in the Redis server.
+//   - RedisSRem: Removes one or more members from a set stored at a key in the Redis server.
+//   - RedisSCard: Returns the number of members in a set stored at a key in the Redis server.
 func SetUPRedisFunctions(table *lua.LTable, L *lua.LState) {
 	table.RawSetString(global.LuaFnRedisGet, L.NewFunction(RedisGet))
 	table.RawSetString(global.LuaFnRedisSet, L.NewFunction(RedisSet))
