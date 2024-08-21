4acb7f478e9544e31975395491b44295d06865e2
Christian Roessner <c@roessner.co>
2024-08-21 14:19:30 +0200

    Add test for Redis SIsMember function
    Introduces unit tests for the Redis `SIsMember` function, including cases where a value exists in the set, does not exist, and errors during member checks. This enhances the test coverage and ensures the function behaves as expected under various scenarios.
    
    Signed-off-by: Christian Roessner <c@roessner.co>

--- server/lualib/redislib/set_test.go
+++ server/lualib/redislib/set_test.go
@@ -111,3 +111,90 @@ func TestRedisSAdd(t *testing.T) {
 		})
 	}
 }
+
+func TestRedisSIsMember(t *testing.T) {
+	tests := []struct {
+		name          string
+		key           string
+		value         any
+		expectedValue lua.LValue
+		expectedErr   lua.LValue
+		setupMock     func(mock redismock.ClientMock)
+	}{
+		{
+			name:          "ExistInSet",
+			key:           "existingKey",
+			value:         "existingValue",
+			expectedValue: lua.LTrue,
+			expectedErr:   lua.LNil,
+			setupMock: func(mock redismock.ClientMock) {
+				mock.ExpectSIsMember("existingKey", "existingValue").SetVal(true)
+			},
+		},
+		{
+			name:          "NotExistInSet",
+			key:           "existingKey",
+			value:         "nonExistingValue",
+			expectedValue: lua.LFalse,
+			expectedErr:   lua.LNil,
+			setupMock: func(mock redismock.ClientMock) {
+				mock.ExpectSIsMember("existingKey", "nonExistingValue").SetVal(false)
+			},
+		},
+		{
+			name:          "ErrOnMemberCheck",
+			key:           "existingKey",
+			value:         "anyValue",
+			expectedValue: lua.LNil,
+			expectedErr:   lua.LString("some error"),
+			setupMock: func(mock redismock.ClientMock) {
+				mock.ExpectSIsMember("existingKey", "anyValue").SetErr(errors.New("some error"))
+			},
+		},
+	}
+
+	L := lua.NewState()
+
+	defer L.Close()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock := redismock.NewClientMock()
+			if db == nil || mock == nil {
+				t.Fatalf("Failed to create Redis mock client.")
+			}
+
+			tt.setupMock(mock)
+			rediscli.ReadHandle = db
+
+			L.SetGlobal("key", lua.LString(tt.key))
+			L.SetGlobal("value", ConvertGoToLuaValue(tt.value))
+
+			globals := L.NewTable()
+
+			SetUPRedisFunctions(globals, L)
+			L.SetGlobal(global.LuaDefaultTable, globals)
+
+			redisSIsMemberFunction := L.GetGlobal(global.LuaDefaultTable).(*lua.LTable).RawGetString(global.LuaFnRedisSIsMember)
+			if redisSIsMemberFunction == nil {
+				t.Fatalf("Function nauthilus.redis_sismember does not exist")
+			}
+
+			err := L.DoString(fmt.Sprintf("result, err = nauthilus.redis_sismember(key, value)"))
+			if err != nil {
+				t.Fatalf("Running Lua code failed: %v", err)
+			}
+
+			gotResult := L.GetGlobal("result")
+			if gotResult.Type() != tt.expectedValue.Type() && gotResult.String() != tt.expectedValue.String() {
+				t.Errorf("nauthilus.redis_sismember() gotResult = %v, want %v", gotResult, tt.expectedValue)
+			}
+
+			gotErr := L.GetGlobal("err")
+
+			checkLuaError(t, gotErr, tt.expectedErr)
+
+			mock.ClearExpect()
+		})
+	}
+}
