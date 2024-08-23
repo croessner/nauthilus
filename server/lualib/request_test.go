e444860ddcdaff6d725a42be576aa4e17e9f8ead
Christian Roessner <c@roessner.co>
2024-08-23 15:54:19 +0200

    Add tests for GetAllHTTPRequestHeaders function
    Introduce a set of test cases to ensure the GetAllHTTPRequestHeaders function behaves correctly with different sets of HTTP headers. This includes cases with single and multiple header values, as well as an empty header scenario.
    
    Signed-off-by: Christian Roessner <c@roessner.co>

--- server/lualib/request_test.go
+++ server/lualib/request_test.go
@@ -8,6 +8,75 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
+func TestGetAllHTTPRequestHeaders(t *testing.T) {
+	testCases := []struct {
+		name           string
+		requestHeaders http.Header
+		expectedKeys   []string
+	}{
+		{
+			name: "AllHeadersSingleValue",
+			requestHeaders: http.Header{
+				"Accept":        []string{"application/json"},
+				"Authorization": []string{"Bearer abc123"},
+				"Cookie":        []string{"session_id=123456"},
+			},
+			expectedKeys: []string{"accept", "authorization", "cookie"},
+		},
+		{
+			name: "AllHeadersMultipleValues",
+			requestHeaders: http.Header{
+				"Cache-Control": []string{"no-cache", "no-store"},
+				"Accept":        []string{"application/json", "text/html"},
+			},
+			expectedKeys: []string{"cache-control", "accept"},
+		},
+		{
+			name:           "EmptyHeaders",
+			requestHeaders: http.Header{},
+			expectedKeys:   []string{},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			L := lua.NewState()
+
+			defer L.Close()
+
+			httpRequest := &http.Request{
+				Header: tc.requestHeaders,
+			}
+
+			lFunc := GetAllHTTPRequestHeaders(httpRequest)
+			lFunc(L)
+
+			lTable := L.CheckTable(-1)
+
+			lengthTable := 0
+			lTable.ForEach(func(_ lua.LValue, _ lua.LValue) {
+				lengthTable++
+			})
+
+			if len(tc.expectedKeys) != lengthTable {
+				t.Errorf("expected number of header keys to be %d, got %d", len(tc.expectedKeys), lTable.Len())
+			}
+
+			foundKeys := make(map[string]bool)
+
+			lTable.ForEach(func(k, _ lua.LValue) {
+				key := fmt.Sprintf("%v", k)
+				foundKeys[key] = true
+			})
+
+			for _, ek := range tc.expectedKeys {
+				if !foundKeys[ek] {
+					t.Errorf("expected header key %s was not found", ek)
+				}
+			}
+		})
+	}
+}
+
 func TestGetHTTPRequestHeader(t *testing.T) {
 	testCases := []struct {
 		name           string
