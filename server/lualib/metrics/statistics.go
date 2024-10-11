e97845b9b8eb53e8c60e801373299166c0696096
Christian Roessner <c@roessner.co>
2024-10-11 09:47:32 +0200

    Feat: Add GaugeVec support to Lua metrics for HTTP client concurrency
    Introduced GaugeVec for monitoring concurrent HTTP requests. Updated Lua plugins and metrics library to create, increment, add, subtract, set, and decrement GaugeVec values. Enhanced Prometheus integration with new metric testing.
    
    Signed-off-by: Christian Roessner <c@roessner.co>

--- server/lualib/metrics/statistics.go
+++ server/lualib/metrics/statistics.go
@@ -10,6 +10,7 @@ var (
 	summaries  = make(map[string]*prometheus.SummaryVec)
 	counters   = make(map[string]*prometheus.CounterVec)
 	histograms = make(map[string]*prometheus.HistogramVec)
+	gauges     = make(map[string]*prometheus.GaugeVec)
 )
 
 // createSummaryVec registers a new Prometheus SummaryVec metric with the provided name, help description, and label names.
@@ -105,6 +106,40 @@ func createHistogramVec(L *lua.LState) int {
 	return 0
 }
 
+// createGaugeVec creates and registers a new GaugeVec with the specified name, help message, and optional label names.
+// The function expects a Lua state, with the first two arguments being the name and help string.
+// If a third argument is provided, it should be a table of label names.
+// If a GaugeVec with the given name already exists, the function does nothing.
+func createGaugeVec(L *lua.LState) int {
+	name := L.CheckString(1)
+	help := L.CheckString(2)
+
+	labelNames := make([]string, 0)
+
+	if L.GetTop() > 2 {
+		labelTable := L.CheckTable(3)
+		labelTable.ForEach(func(_ lua.LValue, value lua.LValue) {
+			labelNames = append(labelNames, value.String())
+		})
+	}
+
+	// Check if the histogram already exists
+	if _, exists := gauges[name]; exists {
+		return 0
+	}
+
+	gauge := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: name,
+		Help: help,
+	}, labelNames)
+
+	prometheus.MustRegister(gauge)
+
+	gauges[name] = gauge
+
+	return 0
+}
+
 // startSumaryTimer starts a Prometheus timer for a specified SummaryVec metric with the provided label values.
 func startSumaryTimer(L *lua.LState) int {
 	name := L.CheckString(1)
@@ -189,6 +224,123 @@ func incrementCounter(L *lua.LState) int {
 	return 0
 }
 
+// addGauge adds a value to a labeled gauge identified by its name.
+// It takes a Lua state (L) as input, with the following arguments expected:
+// - name: a string representing the name of the GaugeVec.
+// - labels: a Lua table containing label key-value pairs.
+// - value: a number representing the amount to add to the gauge.
+func addGauge(L *lua.LState) int {
+	name := L.CheckString(1)
+	value := L.CheckNumber(2)
+	labels := L.CheckTable(3)
+
+	gauge, exists := gauges[name]
+	if !exists {
+		L.ArgError(1, "GaugeVec not found")
+
+		return 0
+	}
+
+	labelValues := make(map[string]string)
+	labels.ForEach(func(key, value lua.LValue) {
+		labelValues[key.String()] = value.String()
+	})
+
+	gauge.With(labelValues).Add(float64(value))
+
+	return 0
+}
+
+// subGauge subtracts a specified value from a GaugeVec identified by name and labeled with the provided labels.
+func subGauge(L *lua.LState) int {
+	name := L.CheckString(1)
+	value := L.CheckNumber(2)
+	labels := L.CheckTable(3)
+
+	gauge, exists := gauges[name]
+	if !exists {
+		L.ArgError(1, "GaugeVec not found")
+
+		return 0
+	}
+
+	labelValues := make(map[string]string)
+	labels.ForEach(func(key, value lua.LValue) {
+		labelValues[key.String()] = value.String()
+	})
+
+	gauge.With(labelValues).Sub(float64(value))
+
+	return 0
+}
+
+// setGauge sets the value of a GaugeVec identified by the given name and labels in Lua.
+func setGauge(L *lua.LState) int {
+	name := L.CheckString(1)
+	value := L.CheckNumber(2)
+	labels := L.CheckTable(3)
+
+	gauge, exists := gauges[name]
+	if !exists {
+		L.ArgError(1, "GaugeVec not found")
+
+		return 0
+	}
+
+	labelValues := make(map[string]string)
+	labels.ForEach(func(key, value lua.LValue) {
+		labelValues[key.String()] = value.String()
+	})
+
+	gauge.With(labelValues).Set(float64(value))
+
+	return 0
+}
+
+// incrementGauge increments the value of a Prometheus GaugeVec metric based on the label values provided.
+func incrementGauge(L *lua.LState) int {
+	name := L.CheckString(1)
+	labels := L.CheckTable(2)
+
+	gauge, exists := gauges[name]
+	if !exists {
+		L.ArgError(1, "GaugeVec not found")
+
+		return 0
+	}
+
+	labelValues := make(map[string]string)
+	labels.ForEach(func(key, value lua.LValue) {
+		labelValues[key.String()] = value.String()
+	})
+
+	gauge.With(labelValues).Inc()
+
+	return 0
+}
+
+// decrementGauge decreases the value of a specified Gauge within a collection of GaugeVec, based on the provided labels.
+func decrementGauge(L *lua.LState) int {
+	name := L.CheckString(1)
+	labels := L.CheckTable(2)
+
+	gauge, exists := gauges[name]
+	if !exists {
+		L.ArgError(1, "GaugeVec not found")
+
+		return 0
+	}
+
+	labelValues := make(map[string]string)
+	labels.ForEach(func(key, value lua.LValue) {
+		labelValues[key.String()] = value.String()
+	})
+
+	gauge.With(labelValues).Dec()
+
+	return 0
+}
+
 var exportsModPrometheus = map[string]lua.LGFunction{
 	global.LuaFnCreateSummaryVec:    createSummaryVec,
 	global.LuaFnCreateCounterVec:    createCounterVec,
@@ -197,6 +349,12 @@ var exportsModPrometheus = map[string]lua.LGFunction{
 	global.LuaFnStartHistogramTimer: startHistogramTimer,
 	global.LuaFnStopTimer:           stopTimer,
 	global.LuaFnIncrementCounter:    incrementCounter,
+	global.LuaFnCreateGaugeVec:      createGaugeVec,
+	global.LuaFNAddGauge:            addGauge,
+	global.LuaFnSubGauge:            subGauge,
+	global.LuaFnSetGauge:            setGauge,
+	global.LuaFnIncrementGauge:      incrementGauge,
+	global.LuaFnDecrementGauge:      decrementGauge,
 }
 
 // LoaderModPrometheus loads the Prometheus module into the given Lua state.
