a9ac99ede37c7080e9aa14b83ffda7786224b87f
Christian Roessner <c@roessner.co>
2025-04-25 09:30:01 +0200

    Feat: Add GeoIP-based country code integration for experimental ML
    Integrates country code as a feature for both authenticated and non-authenticated users when the experimental ML mode is enabled. Retrieves GeoIP data for non-authenticated users via an external service or defaults to "localhost" for localhost IPs. Enhances neural network capabilities by incorporating location-based data.
    
    Signed-off-by: Christian Roessner <c@roessner.co>

--- server/lua-plugins.d/filters/geoip.lua
+++ server/lua-plugins.d/filters/geoip.lua
@@ -138,6 +138,17 @@ function nauthilus_call_filter(request)
                 end
             end
 
+            -- Add country code to neural network if experimental ML is enabled
+            if os.getenv("NAUTHILUS_EXPERIMENTAL_ML") == "true" then
+                dynamic_loader("nauthilus_neural")
+                local nauthilus_neural = require("nauthilus_neural")
+
+                -- Add country code as a feature for authenticated users
+                nauthilus_neural.add_additional_features({
+                    country_code = current_iso_code
+                })
+            end
+
             if response.object and nauthilus_util.is_table(response.object) and response.object.policy_reject then
                 nauthilus_prometheus.increment_counter(N .. "_count", {
                     country = current_iso_code,
@@ -172,7 +183,105 @@ function nauthilus_call_filter(request)
     else
         -- We must restore a failed authentication flag!
         if not request.authenticated then
+            -- Check if experimental ML is enabled first, as all operations are only necessary if it's true
+            if os.getenv("NAUTHILUS_EXPERIMENTAL_ML") == "true" then
+                -- For non-authenticated users, we still need to get the country code
+                if request.client_ip ~= "127.0.0.1" then
+                    dynamic_loader("nauthilus_prometheus")
+                    local nauthilus_prometheus = require("nauthilus_prometheus")
+
+                    dynamic_loader("nauthilus_gluahttp")
+                    local http = require("glua_http")
+
+                    dynamic_loader("nauthilus_gll_json")
+                    local json = require("json")
+
+                    local t = {}
+
+                    t.key = "client"
+                    t.value = {
+                        address = request.client_ip
+                        -- No account for non-authenticated users
+                    }
+
+                    local payload, json_encode_err = json.encode(t)
+                    nauthilus_util.if_error_raise(json_encode_err)
+
+                    nauthilus_prometheus.increment_gauge(HCCR, { service = N })
+
+                    local timer = nauthilus_prometheus.start_histogram_timer(N .. "_duration_seconds", { http = "post" })
+                    local result, request_err = http.post(os.getenv("GEOIP_POLICY_URL"), {
+                        timeout = "10s",
+                        headers = {
+                            Accept = "*/*",
+                            ["User-Agent"] = "Nauthilus",
+                            ["Content-Type"] = "application/json",
+                        },
+                        body = payload,
+                    })
+                    nauthilus_prometheus.stop_timer(timer)
+                    nauthilus_prometheus.decrement_gauge(HCCR, { service = N })
+                    nauthilus_util.if_error_raise(request_err)
+
+                    if result.status_code ~= 202 then
+                        nauthilus_util.if_error_raise(N .. "_status_code=" .. tostring(result.status_code))
+                    end
+
+                    local response, err_jdec = json.decode(result.body)
+                    nauthilus_util.if_error_raise(err_jdec)
+
+                    if response.err == nil then
+                        local current_iso_code = "unknown"
+
+                        if response.object then
+                            -- Try to get the ISO country code
+                            if nauthilus_util.is_table(response.object) then
+                                for key, values in pairs(response.object) do
+                                    if key == "current_country_code" then
+                                        if nauthilus_util.is_string(values) then
+                                            current_iso_code = values
+                                        end
+                                    end
+                                end
+                            end
+                        end
+
+                        -- Add country code to neural network
+                        dynamic_loader("nauthilus_neural")
+                        local nauthilus_neural = require("nauthilus_neural")
+
+                        -- Add country code as a feature for non-authenticated users
+                        -- Using the actual country code retrieved from the GeoIP service
+                        nauthilus_neural.add_additional_features({
+                            country_code = current_iso_code
+                        })
+                    end
+                else
+                    -- For localhost IP, use "localhost" as the country code
+                    dynamic_loader("nauthilus_neural")
+                    local nauthilus_neural = require("nauthilus_neural")
+
+                    -- Add country code as a feature for non-authenticated users with localhost IP
+                    nauthilus_neural.add_additional_features({
+                        country_code = "localhost"
+                    })
+                end
+            end
+
             return nauthilus_builtin.FILTER_REJECT, nauthilus_builtin.FILTER_RESULT_OK
+        else
+            -- This is for authenticated users with localhost IP (127.0.0.1)
+            -- Add country code to neural network if experimental ML is enabled
+            if os.getenv("NAUTHILUS_EXPERIMENTAL_ML") == "true" then
+                dynamic_loader("nauthilus_neural")
+                local nauthilus_neural = require("nauthilus_neural")
+
+                -- Add country code as a feature for authenticated users with localhost IP
+                -- Using "localhost" as the value for localhost IP
+                nauthilus_neural.add_additional_features({
+                    country_code = "localhost"
+                })
+            end
         end
     end
 
