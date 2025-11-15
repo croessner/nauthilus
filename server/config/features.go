ffb87ededa3d74bd5c6520714df6b2953a3c75e8
Christian Roessner <c@roessner.co>
2025-11-15 16:38:05 +0100

    Feat: Add custom hostname validator with optional trailing dot support
    - Introduced `hostname_rfc1123_with_opt_trailing_dot` validation rule to allow optional trailing dots while ensuring RFC1123 compliance.
    - Updated configuration fields (`RBL`, `StaticDomains`, `Host`) to use the new validator.
    - Documented and implemented validation logic under `server/config/server.go`.
    
    Signed-off-by: Christian Roessner <c@roessner.co>

--- server/config/features.go
+++ server/config/features.go
@@ -19,7 +19,7 @@ import "fmt"
 
 type RelayDomainsSection struct {
 	SoftWhitelist `mapstructure:"soft_whitelist"`
-	StaticDomains []string `mapstructure:"static" validate:"required,dive,hostname_rfc1123"`
+	StaticDomains []string `mapstructure:"static" validate:"required,dive,hostname_rfc1123_with_opt_trailing_dot"`
 }
 
 func (r *RelayDomainsSection) String() string {
@@ -52,7 +52,7 @@ func (r *RelayDomainsSection) GetSoftWhitelist() SoftWhitelist {
 
 type BackendServer struct {
 	Protocol      string `mapstructure:"protocol" validate:"required,oneof=imap pop3 lmtp smtp sieve http"`
-	Host          string `mapstructure:"host" validate:"required,hostname_rfc1123|ip"`
+	Host          string `mapstructure:"host" validate:"required,hostname_rfc1123_with_opt_trailing_dot|ip"`
 	DeepCheck     bool   `mapstructure:"deep_check"`
 	RequestURI    string `mapstructure:"request_uri" validate:"omitempty,url_encoded"`
 	TestUsername  string `mapstructure:"test_username" validate:"omitempty,excludesall= "`
